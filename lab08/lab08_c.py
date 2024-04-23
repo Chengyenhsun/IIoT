1b1f4f8cab6def8006f914a49016f9f211a9eb02
Chengyenhsun <f14106155@gs.ncku.edu.tw>
2024-04-23 17:46:50 +0800

    lab08_c

--- lab08/lab08_c.py
+++ lab08/lab08_c.py
@@ -2,13 +2,6 @@ import RPi.GPIO as GPIO
 import time
 import tm1637
 
-# GPIO23 (Pin 16) <-> CLK
-# GPIO24 (Pin 18) <-> DI0
-
-Display = tm1637.TM1637(23, 24, tm1637.BRIGHT_TYPICAL)
-Display.Clear()
-Display.SetBrightnes(1)
-
 GPIO.setmode(GPIO.BCM)
 
 TRIG = 4
@@ -16,6 +9,29 @@ ECHO = 17
 
 GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
+
+DIO_PIN = 24
+CLK_PIN = 23
+
+GPIO.setup(DIO_PIN, GPIO.OUT)
+GPIO.setup(CLK_PIN, GPIO.OUT)
+Display = tm1637.TM1637(23, 24, tm1637.BRIGHT_TYPICAL)
+Display.Clear()
+Display.SetBrightnes(1)
+
+
+def send_data(data):
+    for i in range(8):
+        GPIO.output(DIO_PIN, data & 0x80)
+        GPIO.output(CLK_PIN, True)
+        time.sleep(0.000001)
+        GPIO.output(CLK_PIN, False)
+        data <<= 1
+
+
+def display_number(number):
+    num_str = str(round(number)).zfill(4)  # 将距离值转换为整数，并填充成4位字符串
+    Display.Show([int(num_str[0]), int(num_str[1]), int(num_str[2]), int(num_str[3])])
 
 
 def measure_distance():
@@ -31,35 +47,20 @@ def measure_distance():
 
     while GPIO.input(ECHO) == 0:
         pulse_start = time.time()
-
     while GPIO.input(ECHO) == 1:
         pulse_end = time.time()
 
     pulse_duration = pulse_end - pulse_start
-    distance = pulse_duration * 34300 / 2
-    distance = round(distance, 1)
-
+    distance = round(pulse_duration * 17150, 1)
     return distance
-
-
-def display_distance(distance):
-    # 将距离转换为整数并显示在TM1637上
-    distance_int = int(distance)
-    Display.Show(
-        [
-            distance_int // 1000,
-            (distance_int // 100) % 10,
-            (distance_int // 10) % 10,
-            distance_int % 10,
-        ]
-    )
 
 
 try:
     while True:
         distance = measure_distance()
-        display_distance(distance)
-        time.sleep(1)
+        display_number(distance)
+        print("Distance:", distance, "cm")
+        time.sleep(3)  # 每隔3秒更新一次数据
 except KeyboardInterrupt:
     pass
 finally:
