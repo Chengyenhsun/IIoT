11ecfc7df4cc67393338e30261792898e8e383f3
Iris <F14101655@gs.ncku.edu.tw>
2024-06-23 18:19:11 +0800

    final project

--- final_project/main.py
+++ final_project/main.py
@@ -1,44 +1,32 @@
-import os
+from flask import Flask, render_template, Response
 import cv2
-from flask import Flask, Response, render_template
-
-# 設置環境變數
-os.environ["QT_QPA_PLATFORM"] = "xcb"
 
 app = Flask(__name__)
 
-# 開啟USB攝影機（根據需要調整設備索引）
+# 打開USB攝像頭
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-if not cap.isOpened():
-    print("無法開啟攝影機")
-    exit()
-
-
-def generate_frames():
+def gen_frames():
     while True:
         ret, frame = cap.read()
         if not ret:
             break
         else:
-            ret, buffer = cv2.imencode(".jpg", frame)
+            # 將每一幀影像轉換成byte格式
+            ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
-            yield (
-                b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n"
-            )
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # 產生影像的generator
 
-
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')  # 渲染index.html模板
 
-
-@app.route("/video_feed")
+@app.route('/video_feed')
 def video_feed():
-    return Response(
-        generate_frames(), mimetype="multipart/x-mixed-replace; boundary=frame"
-    )
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
